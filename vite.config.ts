92e2d4e72488011b6b6ad9a0ade4a97a8b0cc425
billy billy <seanhung2000@gmail.com>
2025-03-02 11:33:37 +0800

    refactor: update tailwindcss to v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tsconfigPaths()],
+  plugins: [
+    vue(),
+    tsconfigPaths(),
+    tailwindcss(),
+  ],
 })
